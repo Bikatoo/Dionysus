c19c2fef967183098b54664d1f9d4999c2d8a89b
Bikatoo <1115626649@qq.com>
2022-03-11 21:34:18 +0800

    first commit

---
+++ src/test/java/com/bikatoo/dionysus/dionysus/DionysusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bikatoo.dionysus.dionysus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DionysusApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
